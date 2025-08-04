bce0c5d00153ec39108a0057427cfefd26419f65
Bruno Torres <brunomptorres@gmail.com>
2025-08-04 22:54:17 +0000

    Version 1

---
+++ index.js
@@ -0,0 +1,46 @@
+const preferedDomains = {
+  instagram: "ddinstagram.com",
+  twitter: "fxtwitter.com",
+};
+function modifyClipboard(event) {
+  event.preventDefault();
+
+  let clipboardText = window.getSelection().toString();
+
+  if (clipboardText === "") {
+    clipboardText = window.location.href;
+    if (
+      !clipboardText.match(/(instagram|x|twitter)\.com\/.{4,15}\/status\/\d+/)
+    ) {
+      return;
+    }
+  }
+
+  let modifiedText;
+
+  if (clipboardText.includes("instagram.com")) {
+    modifiedText = clipboardText
+      .replace(/(?:instagram)\.com/g, preferedDomains.instagram)
+      .replace(/\?.*$/, "");
+  }
+
+  if (
+    clipboardText.includes("x.com") ||
+    clipboardText.includes("twitter.com")
+  ) {
+    modifiedText = clipboardText
+      .replace(/(?:twitter|x)\.com/g, preferedDomains.twitter)
+      .replace(/\?.*$/, "");
+  }
+
+  navigator.clipboard
+    .writeText(modifiedText)
+    .then(() => {
+      console.log("Successfully modified clipboard: " + modifiedText);
+    })
+    .catch((error) => {
+      console.error("Failed to modify clipboard: " + error);
+    });
+}
+
+document.addEventListener("copy", modifyClipboard);
